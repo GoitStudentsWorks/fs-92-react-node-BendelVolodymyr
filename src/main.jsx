63fc803ab42f3e45ffc831a76e71329491c42268
GoitStudentsWorks <129747846+GoitStudentsWorks@users.noreply.github.com>
2024-04-18 00:04:34 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -14,7 +14,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="/Smart-Foxes-WaterTracker">
+        <BrowserRouter basename="/fs-92-react-node-BendelVolodymyr">
           <ModalProvider>
             <GlobalStyle />
             <App />
